48d7247b213d94e51233499c03bae2c3fff3a60e
Janice Darikho <janicedarikho@gmail.com>
2018-05-19 01:35:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
   if word.scan(/[aeiou]\w+/)
     true
-  elsif word.scan(/\s[bcdfghjklmnpqrstvwxyz]\w+/)
+  elsif word.scan(/\s([bcdfghjklmnpqrstvwxyz]\w+)/)
     false
   else 
     false
